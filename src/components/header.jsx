38095fc4d3dd270c934c667d76e34abd4acac3c5
William Lumbantobing <william.sl.tobing@gmail.com>
2020-12-17 18:58:27 +0700

    fix header title and video responsiveness

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,19 +6,21 @@ const Header = () => (
   <header id="header-home">
     <div className="container">
       <div className="header-content">
-        <h1>
-          We are
-          <span
-            className="txt-type"
-            data-wait="1000"
-            data-words='["Future", "Shoes", "Future Shoes"]'
-          ></span>
-        </h1>
-        <p className="lead">
-          We are shoes that come from the future to accompany you in exploring
-          forward.
-        </p>
-        <a className="btn-light" href="#gallery">
+        <div className="header-txt">
+          <h1>
+            We are
+            <span
+              className="txt-type"
+              data-wait="1000"
+              data-words='["Future", "Shoes", "Future Shoes"]'
+            ></span>
+          </h1>
+          <p className="lead">
+            Shoes that come from the future to accompany you in exploring
+            forward.
+          </p>
+        </div>
+        <a className="btn-main" href="#gallery">
           View Our Articles
         </a>
       </div>
