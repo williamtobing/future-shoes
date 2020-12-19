74e151e4d013b93ef1f5437550d5d4f33746799f
William Lumbantobing <william.sl.tobing@gmail.com>
2020-12-19 12:33:25 +0700

    fix all raja ongkir api

--- src/components/section/section-c-form.jsx
+++ src/components/section/section-c-form.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import axios from "axios";
-// import Select from "react-select";
+import Select from "react-select";
 
 import "../../scss/main.scss";
 
@@ -10,103 +10,233 @@ class SectionForm extends React.Component {
     super();
 
     this.state = {
-      isSubmitted: false,
-      data: [],
-      origin: [],
+      courierList: ["JNE", "TIKI", "POS"],
+      destination: null,
+      weight: "1000",
+      options: [],
+      cekOngkirResults: [],
+      formInfo: {
+        name: "",
+        email: "",
+        phoneNumber: "",
+        fullAddress: "",
+      },
+      error: "",
     };
   }
+
   componentDidMount() {
     axios
       .get("https://rajaongkir-fshoes.herokuapp.com/city")
-      .then((response) => {
-        const city = response.data.data;
-        this.setState({ data: city });
-        // this.setState({ origin: })
-        // console.log(this.state.data);
+      .then((res) => {
+        this.setState({
+          options: res.data.data.map((data) => {
+            return {
+              value: data.city_id,
+              label: data.city_name,
+            };
+          }),
+        });
+      })
+      .catch((e) => {
+        this.setState({ error: e.response.data });
       });
   }
 
-  handleSubmit() {
-    this.setState({ isSubmitted: true });
+  componentWillUnmount() {
+    this.setState({ error: "" });
   }
 
-  render() {
-    let checkForm;
-    const cities = this.state.data;
-    // const options = cities.map((city) => {
-    //   return city.city_name;
-    // });
-    if (this.state.isSubmitted) {
-      checkForm = (
-        <section id="contact-a" className="text-center py-3">
-          <div className="container">
-            <h2 style={{ margin: "235px 0 220px 0" }} className="section-title">
-              Terkirim
-            </h2>
-          </div>
-        </section>
-      );
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { destination, weight, courierList, formInfo } = this.state;
+    if (
+      destination !== null &&
+      weight !== "" &&
+      formInfo.name !== "" &&
+      formInfo.email !== "" &&
+      formInfo.phoneNumber !== "" &&
+      formInfo.fullAddress !== ""
+    ) {
+      this.setState({ error: "" });
+      courierList.forEach((courier) => {
+        axios
+          .get("https://rajaongkir-fshoes.herokuapp.com/cekongkir", {
+            params: {
+              origin: 465,
+              destination: destination.value,
+              weight: weight,
+              courier: courier.toLowerCase(),
+            },
+          })
+          .then((res) => {
+            let joined = this.state.cekOngkirResults.concat(res.data.data);
+            this.setState({
+              cekOngkirResults: joined,
+            });
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+      });
     } else {
-      checkForm = (
-        <section id="contact-a" className="text-center py-3">
-          <div className="container">
-            <h2 className="section-title">Try Our Product</h2>
-            <div className="bottom-line"></div>
-            <p className="lead">Feel free to fill the form below</p>
-            <form
-              onSubmit={() => {
-                this.handleSubmit();
-              }}
-            >
-              <div className="text-fields">
-                <input
-                  type="text"
-                  className="text-input name-input"
-                  placeholder="Name"
-                  name="name"
-                />
-                {/* <input
-                  type="text"
-                  className="text-input subject-input"
-                  placeholder="City"
-                  name="subject"
-                /> */}
-                <select className="text-input subject-input" name="subject">
-                  <option key="0">City</option>
-                  {cities.map((item) => {
-                    return <option key={item.city_id}>{item.city_name}</option>;
-                  })}
-                </select>
-                <input
-                  type="email"
-                  className="text-input email-input"
-                  placeholder="Email"
-                  name="email"
-                />
-                <input
-                  type="text"
-                  className="text-input phone-input"
-                  placeholder="Phone Number"
-                  name="phone"
-                />
-                <textarea
-                  className="text-input message-input"
-                  placeholder="Enter your full address"
-                  name="message"
-                ></textarea>
-                <div className="my-2">
-                  <div data-netlify-recaptcha="true"></div>
-                </div>
-              </div>
-              <button type="submit" className="btn-dark">
-                Submit
-              </button>
-            </form>
-          </div>
-        </section>
-      );
+      this.setState({
+        error: "All field must be filled in or the type must be right",
+      });
     }
-    return <>{checkForm}</>;
+  };
+
+  handleChangeDestination = (destination) => {
+    this.setState({ destination });
+  };
+
+  render() {
+    const {
+      formInfo,
+      destination,
+      cekOngkirResults,
+      weight,
+      options,
+      error,
+    } = this.state;
+    return (
+      <section id="contact-a" className="text-center py-3">
+        <div className="container">
+          <h2 className="section-title">Try Our Product</h2>
+          <div className="bottom-line"></div>
+          <p className="lead">Feel free to fill the form below</p>
+
+          {error && <h3 style={{ color: "red" }}>{error}</h3>}
+
+          <>
+            {cekOngkirResults.length > 0 ? (
+              <>
+                <div style={{ textTransform: "none" }}>
+                  <h4 style={{ textTransform: "capitalize" }}>
+                    Name: {formInfo.name}
+                  </h4>
+                  <h4 style={{ textTransform: "lowercase" }}>
+                    Email: {formInfo.email}
+                  </h4>
+                  <h4>Phone: {formInfo.phoneNumber}</h4>
+                  <h5>Full Address: {formInfo.fullAddress}</h5>
+                </div>
+
+                <div style={{ padding: "50px" }}>
+                  <div className="Header">
+                    From: Tapanuli Utara To: {destination.label} Berat {weight}{" "}
+                    gram
+                  </div>
+                  <table id="customers">
+                    <thead>
+                      <tr>
+                        <th>Kurir</th>
+                        <th>Jenis</th>
+                        <th>Tarif</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {cekOngkirResults.map((result, key) => {
+                        return (
+                          <tr key={key}>
+                            {result.results[0]?.code ? (
+                              <td style={{ textTransform: "uppercase" }}>
+                                {result.results[0]?.code}
+                              </td>
+                            ) : (
+                              <td>Not Supported</td>
+                            )}
+                            {result.results[0]?.costs[0]?.service ? (
+                              <td>{result.results[0]?.costs[0]?.service}</td>
+                            ) : (
+                              <td>Not Supported</td>
+                            )}
+                            {result.results[0]?.costs[0]?.cost[0]?.value ? (
+                              <td>
+                                Rp {result.results[0]?.costs[0]?.cost[0]?.value}
+                              </td>
+                            ) : (
+                              <td>Not Supported</td>
+                            )}
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                </div>
+              </>
+            ) : (
+              <form onSubmit={this.handleSubmit}>
+                <div className="text-fields">
+                  <input
+                    type="text"
+                    className="text-input name-input"
+                    placeholder="Name"
+                    autoComplete="off"
+                    name="name"
+                    value={formInfo.name}
+                    onChange={(e) =>
+                      this.setState({
+                        formInfo: { ...formInfo, name: e.target.value },
+                      })
+                    }
+                  />
+
+                  <Select
+                    value={destination}
+                    onChange={this.handleChangeDestination}
+                    options={options}
+                    placeholder="City"
+                  />
+
+                  <input
+                    type="email"
+                    className="text-input email-input"
+                    autoComplete="off"
+                    placeholder="Email"
+                    name="email"
+                    value={formInfo.email}
+                    onChange={(e) =>
+                      this.setState({
+                        formInfo: { ...formInfo, email: e.target.value },
+                      })
+                    }
+                  />
+                  <input
+                    type="text"
+                    className="text-input phone-input"
+                    autoComplete="off"
+                    placeholder="Phone Number"
+                    name="phoneNumber"
+                    value={formInfo.phoneNumber}
+                    onChange={(e) =>
+                      this.setState({
+                        formInfo: { ...formInfo, phoneNumber: e.target.value },
+                      })
+                    }
+                  />
+                  <textarea
+                    className="text-input message-input"
+                    placeholder="Enter your full address"
+                    autoComplete="off"
+                    name="fullAddress"
+                    value={formInfo.fullAddress}
+                    onChange={(e) =>
+                      this.setState({
+                        formInfo: { ...formInfo, fullAddress: e.target.value },
+                      })
+                    }
+                  ></textarea>
+                </div>
+                <button type="submit" className="btn-dark">
+                  Submit
+                </button>
+              </form>
+            )}
+          </>
+        </div>
+      </section>
+    );
   }
 }
 
